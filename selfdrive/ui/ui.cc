c60695f02038b112334cd45a9510145d1ff27f5f
multikyd <multikyd@gmail.com>
2025-05-01 22:10:18 +0900

    don't run navi app without net connection

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -534,7 +534,7 @@ static void update_status(UIState *s) {
 
   //opkr navi on boot
   if (!s->scene.navi_on_boot && (s->sm->frame - s->scene.started_frame > 5*UI_FREQ)) {
-    if (params.getBool("OpkrRunNaviOnBoot") && params.getBool("ControlsReady") && (params.get("CarParams").size() > 0)) {
+    if (params.getBool("OpkrRunNaviOnBoot") && params.getBool("ControlsReady") && (params.get("CarParams").size() > 0) && s->scene.deviceState.getConnectName() != "---") {
       s->scene.navi_on_boot = true;
       s->scene.map_is_running = true;
       s->scene.map_on_top = true;
